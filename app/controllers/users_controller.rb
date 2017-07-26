4c004e749d0196542e729e59f27b2be691538b2f
Sue <sue-9270@hotmail.com>
2017-07-26 17:47:36 +0800

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      sign_in @user
+      redirect_to users_path
+    else
+      render template: "users/new"
     end
+
   end
 
   # PATCH/PUT /users/1
