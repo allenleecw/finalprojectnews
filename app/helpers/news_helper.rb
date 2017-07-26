3bc3864f6b42e1922e12152087d19a0f10347b35
Allen <mabelsia@Mabels-MacBook-Air.local>
2017-07-26 17:32:06 +0800

    grpahs

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,36 @@
 module NewsHelper
+  def proper_data(positive_hash, negative_hash)
+    new_positive = []
+    new_negative = []
+
+    positive_hash.each do |k , v|
+      if negative_hash[k] == nil
+        new_negative << [k, 0.0]
+      end
+    end
+
+    negative_hash.each do |k, v|
+      new_negative << [k, v]
+    end
+
+    negative_hash.each do |k, v|
+      if positive_hash[k] == nil
+        new_positive << [k, 0.0]
+      end
+    end
+
+    positive_hash.each do |k, v|
+      new_positive << [k, v]
+    end
+
+    new_positive = new_positive.sort_by do | array |
+      array[0]
+    end
+
+    new_negative = new_negative.sort_by do | array |
+      array[0]
+    end
+
+    {"positive" => new_positive, "negative" => new_negative}
+  end
 end
