9300c27accd94b65a207a00a2682b0558e42e490
Sue <sue-9270@hotmail.com>
2017-07-27 16:47:29 +0800

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require Chart.bundle
 //= require chartkick
+
